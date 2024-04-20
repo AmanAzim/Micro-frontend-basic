4247a3954bfe15f200d9c014be9953e69db60a4b
a.azim <a.azim@sevensenders.com>
2024-04-20 19:01:23 +0200

    micro frontend intro

---
+++ home/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./index.scss";
+
+function Header() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+      Only the best spinners
+    </div>
+  );
+}
+
+export default Header;
